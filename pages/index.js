109adad8eb7c6a97a7e1f0c482f0652ef4145766
“Ole <oamp@live.com>
2020-02-06 20:18:43 +0100

    Linked domain, placeholder text

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 function HomePage() {
-    return <div>
-        <h1>Welcome to Next.js!</h1>
-        <p>Thomas er kul, and this website was built and exported for use on Netlify.</p>
-        <img src="https://brands-a.prod.onewp.net/app/uploads/sites/2/2017/12/Troika-66g-600x600.png"
-            alt="Cameron's favourite chocolate" />
-        <p>This change was made on dev branch, if it's live on website it was successfully pulled into master</p>
-    </div>
+
+    const styles = {
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+
+    return (
+        <div style={styles}>
+            <h1>Stay Tuned.</h1>
+        </div>
+    )
 }
 
 export default HomePage
