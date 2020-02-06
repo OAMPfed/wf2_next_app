5a1168746e2f3ba8109e71b37024b8889ba70c67
“Ole <oamp@live.com>
2020-02-06 14:45:51 +0100

    Hey Cameron, this is for you

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ function HomePage() {
     return <div>
         <h1>Welcome to Next.js!</h1>
         <p>Thomas er kul, and this website was built and exported for use on Netlify.</p>
+        <img src="https://brands-a.prod.onewp.net/app/uploads/sites/2/2017/12/Troika-66g-600x600.png"
+            alt="Cameron's favourite chocolate" />
     </div>
 }
 
