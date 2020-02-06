a9a4b0e7161669d9844653f07d7691d95eb03f36
“Ole <oamp@live.com>
2020-02-06 14:50:43 +0100

    Testing dev branch

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ function HomePage() {
         <p>Thomas er kul, and this website was built and exported for use on Netlify.</p>
         <img src="https://brands-a.prod.onewp.net/app/uploads/sites/2/2017/12/Troika-66g-600x600.png"
             alt="Cameron's favourite chocolate" />
+        <p>This change was made on dev branch, if it's live on website it was successfully pulled into master</p>
     </div>
 }
 
