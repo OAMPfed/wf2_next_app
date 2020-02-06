7d1c796a26e78036daccbc7167721476ef805074
“Ole <oamp@live.com>
2020-02-06 14:42:52 +0100

    proof of netlify awesomeness

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
+    return <div>
+        <h1>Welcome to Next.js!</h1>
+        <p>Thomas er kul, and this website was built and exported for use on Netlify.</p>
+    </div>
 }
 
 export default HomePage
