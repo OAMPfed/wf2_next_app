d0b25416a1f38937552db514e4dc53e31d83112a
“Ole <oamp@live.com>
2020-02-08 13:15:41 +0100

    Added about page

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+function AboutPage() {
+
+    const styles = {
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+
+    return (
+        <div style={styles}>
+            <h1>About page.</h1>
+        </div>
+    )
+}
+
+export default AboutPage
